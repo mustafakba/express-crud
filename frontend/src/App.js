a02b5a875a0264703a761a056c98535e85d86390
mustafakba <kaba.mustafaa@gmail.com>
2023-03-28 14:32:46 +0300

    project finished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { Header } from "./components/Header";
+// import { Home } from "./pages/home/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import { AddEdit } from "./pages/addedit/AddEdit";
+import { ToastContainer } from "react-toastify";
+import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
+import Home from "./pages/home/Home";
+import {AddEdit} from "./pages/addedit/AddEdit";
+// import { View } from "./pages/view/View";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+          <Header />
+          <div className="container">
+              <Routes>
+                  <Route path="/" element={<Home/>} />
+                  <Route path="/add" element={<AddEdit/>} />
+              </Routes>
+          </div>
+      </Router>
   );
 }
 
---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,58 @@
+import React, {useState,useEffect} from 'react';
+import './home.css'
+import axios from 'axios'
+const Home = () => {
+    const [data,setData] = useState([])
+
+    useEffect (()=>{
+        getUsers()
+    },[])
+    const getUsers = async () => {
+        const res = await axios.get("http://localhost:5000/users")
+        if(res.status === 200) {
+            setData(res.data)
+        }
+        console.log(data)
+    }
+
+    return (
+        <div className="table-wrapper">
+            <table>
+                <thead>
+                 <tr>
+                     <th>No</th>
+                     <th>Name</th>
+                     <th>Email</th>
+                     <th>Country</th>
+                     <th>Contact</th>
+                     <th>Action</th>
+                 </tr>
+                </thead>
+                <tbody>
+
+                {data && data.map((user,index) =>
+                    <tr key={user.id}>
+                        <td>{index + 1}</td>
+                        <td>{user.name}</td>
+                        <td>{user.mail}</td>
+                        <td>{user.country}</td>
+                        <td>{user.contact}</td>
+                        <td>
+                            <div className="buttons">
+                                <button className="btn btn-primary">View</button>
+                                <button className="btn btn-success">Edit</button>
+                                <button className="btn btn-danger">Delete</button>
+                            </div>
+                        </td>
+                    </tr>
+                )}
+
+
+
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default Home;
